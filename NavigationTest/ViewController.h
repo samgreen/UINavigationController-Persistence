4c451da3a61192c9dc4a3727cd933af767c34d5b
Sam Green <greensamuelm@gmail.com>
2012-08-01 16:32:43 -0400

    Initial Commit

---
+++ NavigationTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NavigationTest
+//
+//  Created by Sam Green on 8/1/12.
+//  Copyright (c) 2012 Sam Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ NavigationTest/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  NavigationTest
+//
+//  Created by Sam Green on 8/1/12.
+//  Copyright (c) 2012 Sam Green. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+@end
